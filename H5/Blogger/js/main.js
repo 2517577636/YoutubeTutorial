dae2a93a9ac738b39661e11981aa22993a8de312
Justin <2517577636@qq.com>
2020-01-02 19:16:02 +0800

    blog carousel

--- H5/Blogger/js/main.js
+++ H5/Blogger/js/main.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
+
+    /*toggle menu click event*/ 
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
+    })
+
+    /*owl-crousel for blog*/
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
     })
 })
