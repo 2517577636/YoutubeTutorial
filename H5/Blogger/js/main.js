52c2a37527055eb8060e3e9ee043d9d764be06e1
Justin <2517577636@qq.com>
2020-01-03 10:54:29 +0800

    website Ccommplte

--- H5/Blogger/js/main.js
+++ H5/Blogger/js/main.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
 
-    /*toggle menu click event*/ 
-    $toggleCollapse.click(function(){
+    /*toggle menu click event*/
+    $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
 
@@ -15,7 +30,16 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        // responsive: responsive
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     })
+
+    //click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    AOS.init();
 })
